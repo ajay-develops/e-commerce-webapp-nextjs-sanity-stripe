b216403b6e38e731e14319a785b8b88c6fe3e1b7
templar-ajay <templar.ajay@gmail.com>
2025-07-18 00:40:01 +0530

    add: loader

---
+++ src/app/(store)/loading.tsx
@@ -0,0 +1,10 @@
+import { BarLoader } from "react-spinners";
+function Loading() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <BarLoader color="#0083ff" />;
+    </div>
+  );
+}
+
+export default Loading;
