062469056a0c9d0a640ac83dd0b5b524ad9f99e4
templar-ajay <templar.ajay@gmail.com>
2025-07-19 11:05:07 +0530

    syntax fixes, eslint errors, query etc.

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -5,22 +5,21 @@ import { redirect, RedirectType } from "next/navigation";
 const SearchPage = async ({
   searchParams,
 }: {
-  searchParams: {
-    query: string;
-  };
+  searchParams: Promise<{ query?: string }>;
 }) => {
   const { query } = await searchParams;
 
-  if (!query.length) {
+  // Check if query exists and has length
+  if (!query || !query.length) {
     redirect("/", RedirectType.push);
   }
 
   const matchedProducts = await searchProductsByName(query);
 
-  if (!matchedProducts.length) {
+  if (!Boolean(matchedProducts?.length)) {
     return (
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
-        <div className="bg-white p-8 roudned-lg shadow-md w-full max-w-4xl">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
           <h1 className="text-3xl font-bold mb-6 text-center">
             No Products found for: {query}
           </h1>
@@ -38,7 +37,11 @@ const SearchPage = async ({
         <h1 className="text-3xl font-bold mb-6 text-center">
           Search results for "{query}"
         </h1>
-        <ProductGrid products={matchedProducts} />
+        {matchedProducts ? (
+          <ProductGrid products={matchedProducts} />
+        ) : (
+          <div>No Products Matched</div>
+        )}
       </div>
     </div>
   );
