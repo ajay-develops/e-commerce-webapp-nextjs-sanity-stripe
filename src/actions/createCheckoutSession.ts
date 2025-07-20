02daf62abcf99910e6ffb64915d19705a7ef4dff
templar-ajay <templar.ajay@gmail.com>
2025-07-20 22:34:56 +0530

    update: success url

--- src/actions/createCheckoutSession.ts
+++ src/actions/createCheckoutSession.ts
@@ -39,10 +39,7 @@ export async function createCheckoutSession(
       customerId = customers.data[0].id;
     }
 
-    const baseUrl =
-      process.env.NODE_ENV === "production"
-        ? `https://${process.env.VERCEL_URL}`
-        : process.env.NEXT_PUBLIC_BASE_URL;
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
     const success_url = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
 
