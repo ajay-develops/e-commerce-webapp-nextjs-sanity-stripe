a2493ff71f16358e118c25ff5b5aaaf68bc0ec64
templar-ajay <templar.ajay@gmail.com>
2025-07-20 21:51:44 +0530

    remove semi-colon from loader

--- src/app/(store)/loading.tsx
+++ src/app/(store)/loading.tsx
@@ -2,7 +2,7 @@ import { BarLoader } from "react-spinners";
 function Loading() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <BarLoader color="#0083ff" />;
+      <BarLoader color="#0083ff" />
     </div>
   );
 }
